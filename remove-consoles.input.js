36522019fa9e7968e611ae0b79325df8145db3e1
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2021-09-13 17:45:49 +0300

    Test codemods with jscodeshift

---
+++ remove-consoles.input.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+export const sum = (a, b) => {
+  return a + b;
+};
+
+export const multiply = (a, b) => {
+  return a * b;
+};
+
+export const divide = (a, b) => {
+  return a / b;
+};
+
+export const average = (a, b) => {
+  return divide(sum(a, b), 2);
+};
---
+++ remove-consoles.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default (fileInfo, api) => {
+  const j = api.jscodeshift;
+
+  const root = j(fileInfo.source);
+
+  const callExpressions = root.find(j.CallExpression, {
+    callee: {
+      type: 'MemberExpression',
+      object: { type: 'Identifier', name: 'console' },
+    },
+  }
+  );
+
+  callExpressions.remove();
+
+  return root.toSource();
+};
